a60d351311db98e1118e0c3e08e9f325e33439a4
罗凯腾 <luokaiteng@eastmoney.com>
2023-11-28 10:47:53 +0800

    chore: init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const data = ref<string>("123")
+</script>
+
+<template>
+  <div class="container p-2 flex w-full h-full">
+    <div></div>
+  </div>
+</template>
